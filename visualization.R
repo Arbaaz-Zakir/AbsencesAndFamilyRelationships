771f520f95c0d9804e43e56eb6fdbe99be71842b
Arbaaz Zakir <z.arbaaz786@gmail.com>
2020-12-09 13:27:55 +0000

    added methods for installing and using tidyverse package, and plot means on boxplot. still needs to be presented for review on thursday, also still have not recived work from other group members who should be doing this task

--- visualization.R
+++ visualization.R
@@ -1,16 +1,22 @@
+install.packages("tidyverse")
+library(tidyverse)
+pdf("visualization.pdf")
+
 d <- read.csv("student-mat.csv")
 relevantdata <- d %>% select(famrel, absences)
 
-x <- newdf$famrel
-y <- newdf$absences
+#x <- newdf$famrel
+#y <- newdf$absences
 x <- relevantdata$famrel
 y <- relevantdata$absences
 
-pdf("visualization.pdf")
+M = tapply(relevantdata$absences, INDEX = relevantdata$famrel, FUN= mean)
+
 
 boxplot(y ~ x, main = "family relationships v absences", xlab = "family relationships", ylab = "absences", pch = 19, frame = T)
-points(y~x, col="red")
+points(y~x, col="cyan")
 
 
+points(M,col="red",pch="+",cex=2)
 dev.off()
 
