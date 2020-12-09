ac58972a2ea910c7055ceda3aeee68c085ad0180
Arbaaz Zakir <z.arbaaz786@gmail.com>
2020-12-09 22:24:14 +0000

    added legend and cleaned up some code, removing refundant lines of code that were commented out

--- visualization.R
+++ visualization.R
@@ -1,22 +1,24 @@
 install.packages("tidyverse")
 library(tidyverse)
-pdf("visualization.pdf")
+pdf("visualization.pdf", width = 10, height = 10)
 
 d <- read.csv("student-mat.csv")
 relevantdata <- d %>% select(famrel, absences)
 
-#x <- newdf$famrel
-#y <- newdf$absences
 x <- relevantdata$famrel
 y <- relevantdata$absences
 
 M = tapply(relevantdata$absences, INDEX = relevantdata$famrel, FUN= mean)
 
 
-boxplot(y ~ x, main = "family relationships v absences", xlab = "family relationships", ylab = "absences", pch = 19, frame = T)
+boxplot(y ~ x, main = "Boxplot to Show Distribution In Recorded Absences (Y axis) Between Students Who Report 
+        Different Quality of Family Relationships (1 - very bad to 5 - excellent) (X - axis)",
+        xlab = "Self Reported Family Relationships (1 - very bad to 5 - excellent)", ylab = "Number of Recorded Absences", pch = 19, frame = T)
+
 points(y~x, col="cyan")
-
-
 points(M,col="red",pch="+",cex=2)
+
+legend(x="topright", legend=c("Data points", "Mean", "Outliers"), col=c("cyan", "red", "black"), pch = c(1, 3, 19))
+
 dev.off()
 
